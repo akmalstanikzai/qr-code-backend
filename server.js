192fd8b3bcbe7768cc80fe5f73b8a9a4f90255bb
akmalstanikzai <akmalstanikzai.af@gmail.com>
2025-12-19 05:57:06 +0500

    Improvements

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/user');
 
-
 // Express app
 const app = express();
 
@@ -19,14 +18,16 @@ app.use((req, res, next) => {
 // Routes
 app.use('/api/users', userRoutes);
 
+// Port fallback (CRITICAL for Docker/Jenkins)
+const PORT = process.env.PORT || 5000;
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to the database');
 
-    // Start the server
-    app.listen(process.env.PORT, () => {
-      console.log('Listening for requests on port', process.env.PORT);
+    app.listen(PORT, () => {
+      console.log('Listening for requests on port', PORT);
     });
   })
   .catch((err) => {
